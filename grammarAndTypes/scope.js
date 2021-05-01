8d20e93bb777cb3c43ca294b806ac70887a29186
David Nieto <davidreynaldonieto@gmail.com>
2021-05-01 15:59:10 -0400

    work on LCF challenge

--- grammarAndTypes/scope.js
+++ grammarAndTypes/scope.js
@@ -32,21 +32,23 @@
 //        }
 
 //  1. Write a for loop that counts from 0-10. Each time the loop runs, it should invoke a function and accept the data from the for loop as a parameter(see step 2).
-
+let num;
 for(let k=0; k<=10; k++){
-    console.log(k);
-    callback(num);
+    num=k;
+    console.log(num);
+    if(`${num}` % 1 != 0 ){
+        console.log("What is this?");
+    }else if((num/2) % 1 === 0){
+        console.log("The number is even.");
+    }else{console.log("The number is odd.");
+    }
 }
+
+
 
 //  2. Declare a function. Inside the function, console.log each number (0-10) from the previously written for loop. Your console should look like the screenshot below.
 
-let callback = (num) =>{
-    num = k;
-    if((num/2)% 1 === 0){
-        console.log("the number is even");
-    }else{console.log("the number is odd");
-}
-}
+
 
 //  3. Inside of the function, write a conditional that checks if the number is even or odd. If the number is even, console.log 'the number is even'. If the number is odd, console.log 'the number is odd'. Your console should now look like the screenshot below.
 
